cf2c40c53cc445cc7b9f79fe958e252b668d927b
Thomas Mørch <thomas@bowmo.dk>
2015-09-16 23:11:16 +0200

    Added presentation support to encapsulate

--- encapsulate/encapsulate.js
+++ encapsulate/encapsulate.js
@@ -4,13 +4,32 @@ module.exports = function(RED) {
         this.nodeid = config.nodeid;
         this.childid = config.childid;
         this.subtype = config.subtype;
+        this.msgtype = config.msgtype;
+        this.ack = config.ack;
+        this.presentation = config.presentation;
+        this.presentationtext = config.presentationtext;
+        this.presentationtype = config.presentationtype;
         var node = this;
+
+        if (this.presentation) {
+            setTimeout(function() {
+                var msg = {};
+                msg.payload = node.presentationtext;
+                msg.nodeId = node.nodeid;
+                msg.childsensorId = node.childid;
+                msg.subType = node.presentationtype;
+                msg.messageType = 0;
+                msg.ack = 0;
+                node.send(msg);
+            }, 1000);
+        }
+
         this.on('input', function(msg) {
             msg.nodeId = node.nodeid;
             msg.childSensorId = node.childid;
             msg.subType = node.subtype;
-            msg.messageType = 1;
-            msg.ack = 0;
+            msg.messageType = node.msgtype;
+            msg.ack = (node.ack?1:0);
             node.send(msg);
         });
 
