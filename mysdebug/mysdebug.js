ff5c9e4932a6e694c695f090d65c0d6bb53ff6ae
Thomas Mørch <thomas@bowmo.dk>
2015-11-23 14:35:16 +0100

    Rewrote the debug decoder function to be much simpler..

--- mysdebug/mysdebug.js
+++ mysdebug/mysdebug.js
@@ -16,23 +16,37 @@ module.exports = function(RED) {
                 var ack = parseInt(tokens[3]);
                 var subType = parseInt(tokens[4]);
                 var payload = tokens[5];
-                if (messageType==3) {
-                    if(subType==9) {
-                        msg.payload = payload;
-                    } else {
-                        msg.payload = "INTERNAL :SensorId=" + nodeId + ":ChildId="+mys_common.internal[subType].htext + ":Payload=" + payload;
-                    }
+                var subTypeText = null;
+                var msgHeader = '';
+                var msgSubType = null;
+                switch (messageType) {
+                    case 0:
+                        msgHeader = "PRESENTATION";
+                        msgSubType = mys_common.presentation[subType].htext;
+                        break;
+                    case 1:
+                        msgHeader = "SET";
+                        msgSubType = mys_common.subtype[subType].htext;
+                        break;
+                    case 2:
+                        msgHeader = "GET";
+                        msgSubType = mys_common.subtype[subType].htext;
+                        break;
+                    case 3:
+                        if (subType == 9) msg.payload = "GW Debug;" + payload;
+                        else {
+                            msgHeader = "INTERNAL";
+                            msgSubType = mys_common.internal[subType].htext;
+                        }
+                        break;
+                    default:
+                        msg.payload = "unsupported msgType " + messageType; 
+                        break;   
+                                                                       
                 }
-                if (messageType==0) {
-                    msg.payload = "PRESENTATION :SensorId=" + nodeId + ":ChildId=" + childSensorId+":"+mys_common.presentation[subType].htext+":Payload="+payload
-                }                            
-                if (messageType==1 || messageType==2) {
-                    msg.payload = "";
-                    if (messageType==1) msg.payload = "SET :";
-                    else msg.payload = "GET :";
-                    msg.payload = msg.payload + "SensorId="+ nodeId + ":ChildId=" + childSensorId + ":" + mys_common.subtype[subType].htext + ":Payload=" + payload;
+                if (msgSubType != null) {
+                    msg.payload = msgHeader + ";nodeId:" + nodeId + ";childId:"+ childSensorId +";SubType:"+ msgSubType + ";ACK:"+ ack + ";Payload:"+ payload;
                 }
-                
                 
             }
             if (msg.payload != null) node.send(msg);
